e55cded4d6af6bac54aebee819ef6675c55f0984
Eren AVAR <eren.avar@hotmail.com>
2024-05-27 17:03:50 +0200

    implement react navigation dependencies and create screens

---
+++ src/navigation /MainNavigation.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FilmsListScreen from '../screens/FilmsListScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+export default function MainNavigation() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="FilmList" component={FilmsListScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({})
