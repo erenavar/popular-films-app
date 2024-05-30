330d6a2d510672b38a1c306f3b544fd789207910
Eren AVAR <eren.avar@hotmail.com>
2024-05-30 10:03:49 +0200

    add film card css

--- src/components/FilmCard.tsx
+++ src/components/FilmCard.tsx
@@ -1,31 +1,56 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React, { FC } from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React, { FC } from "react";
 
 interface Props {
-    title:string,
-    img:string,
-    year:string
+  title: string;
+  img: string;
+  year: string;
 }
 
-export default function FilmCard (props:Props) {
+export default function FilmCard(props: Props) {
   return (
-    <View>
-      <Image
-      style={styles.image}
-        source={{uri:"https://image.tmdb.org/t/p/w500/"+props.img}}
-      />
-      <Text style={styles.filmName}>{props.title}</Text>
-      <Text style={styles.filmName}>{props.year}</Text>
+    <View style={styles.container}>
+      <View style={styles.dataWrapper}>
+        <Image
+          style={styles.image}
+          source={{ uri: "https://image.tmdb.org/t/p/w500/" + props.img }}
+        />
+        <View style={styles.highlights}>
+          <Text numberOfLines={4} style={styles.filmName}>{props.title}</Text>
+          <Text style={styles.year}>{props.year}</Text>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    image: {
-      height:50,
-      width:50
-    },
-    filmName:{
-        color:"white"
-    }
-})
+  container: {
+    marginBottom: 10
+  },
+  dataWrapper: {
+
+    flexDirection: "row"
+  },
+  image: {
+    height: 150,
+    width: 150,
+  },
+  highlights: {
+    flexShrink: 1,
+    flexDirection: "column",
+    marginLeft: 6,
+    width: "40%"
+  },
+  filmName: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  year: {
+    color: "gray",
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: 2
+  }
+});
--- src/screens/popularFilms/FilmsListScreen.tsx
+++ src/screens/popularFilms/FilmsListScreen.tsx
@@ -7,21 +7,21 @@ import FilmCard from '../../components/FilmCard'
 export default function FilmsListScreen() {
 
 
-    const {data} = useQuery<FilmResponse>({
-        queryKey:['populerFilms'],
-        queryFn: async () => {
-          const response = await fetch("https://api.themoviedb.org/3/movie/popular",{
-            method:"GET",
-            headers:{
-              accept: 'application/json',
-              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTliM2U2OGY5YzQ0OGRjNDljY2JmMDEzMWI5N2Y0OCIsInN1YiI6IjY2NGNlZjhmYmY3YmFlZWU3Y2NkZDQ2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WHSBp9dcu_icb7MBwIRg5D6Dbx7uYNyq-Bs68tBTZE4'
-            }
-          })
-          return await response.json();
+  const { data } = useQuery<FilmResponse>({
+    queryKey: ['populerFilms'],
+    queryFn: async () => {
+      const response = await fetch("https://api.themoviedb.org/3/movie/popular", {
+        method: "GET",
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTliM2U2OGY5YzQ0OGRjNDljY2JmMDEzMWI5N2Y0OCIsInN1YiI6IjY2NGNlZjhmYmY3YmFlZWU3Y2NkZDQ2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WHSBp9dcu_icb7MBwIRg5D6Dbx7uYNyq-Bs68tBTZE4'
         }
-    })
+      })
+      return await response.json();
+    }
+  })
 
-    const numberOfFilms:number = data?.results?.length ?? 0;
+  const numberOfFilms: number = data?.results?.length ?? 0;
 
   return (
     <View style={styles.container}>
@@ -30,40 +30,40 @@ export default function FilmsListScreen() {
         <Pressable style={styles.filterButton}><Text style={styles.filterText}>Filter</Text></Pressable>
       </View>
 
+
       <FlatList
         data={data?.results}
-        renderItem={({item}) => <FilmCard title={item.title} img={item.poster_path} year={item.release_date}/>}
-
+        renderItem={({ item }) => <FilmCard title={item.title} img={item.poster_path} year={item.release_date} />}
       />
-   
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{flex:1,backgroundColor:"black"},
+  container: { flex: 1, backgroundColor: "black" },
 
-  title:{
+  title: {
     flexDirection: "row",
-    justifyContent:"space-between",
-    alignItems:"center",
-    padding:20,
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
   },
-  titleText:{
-    color:"lightgray",
-    fontSize:18
+  titleText: {
+    color: "lightgray",
+    fontSize: 18
   },
   filterButton: {
-    backgroundColor:"gray",
+    backgroundColor: "gray",
     padding: 10,
     borderRadius: 20,
-    width:"30%",
-    justifyContent:"center",
-    alignItems:"center"
+    width: "30%",
+    justifyContent: "center",
+    alignItems: "center"
   },
   filterText: {
-    color:"white",
-    fontSize:18,
-    fontWeight:"bold"
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold"
   }
 })
